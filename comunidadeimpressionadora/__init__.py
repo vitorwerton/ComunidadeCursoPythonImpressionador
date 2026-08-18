63c81fafcd18226e6348035c500986ac70d5daad
Vitor Werton <68294834+vitorwerton@users.noreply.github.com>
2026-08-17 22:56:14 -0300

    Update database configuration to use environment variable
    Added support for DATABASE_URL environment variable to configure SQLAlchemy database URI.

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '3f9a3c2b8f1c0241a47797318454e381'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
