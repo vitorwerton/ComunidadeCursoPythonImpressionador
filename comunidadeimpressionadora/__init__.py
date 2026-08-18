c272f491984d6590094df077e04e02622036d0f8
Vitor Werton <68294834+vitorwerton@users.noreply.github.com>
2026-08-17 22:40:23 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3f9a3c2b8f1c0241a47797318454e381'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import routes
